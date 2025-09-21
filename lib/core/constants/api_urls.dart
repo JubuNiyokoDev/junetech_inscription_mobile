8003aabc32d5b0136d270905756952ec102e08d8
Niyondiko Joffre <niyondikojoffreasjubu@gmail.com>
2025-09-21 13:14:34 +0200

    before delete it local

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,5 +1,5 @@
 class ApiUrls {
-  static const String baseUrl = 'https://junetech-inscription.onrender.com';
+  static const String baseUrl = 'http://168.231.77.200:8000';
   static const String scanCreateUrl = '$baseUrl/api/scans/';
   static const String scanSummaryUrl = '$baseUrl/api/scan-summary/';
   static const String badgeGenerateUrl = '/api/registrations/';
